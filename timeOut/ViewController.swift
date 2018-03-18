2b34d672d0ffda8bc8b908e48ae039923427d7e9
roman rozenblat <romanrozenblat@romans-macbook-air.local>
2018-03-18 15:45:25 -0400

    Initial Commit

---
+++ timeOut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  timeOut
+//
+//  Created by roman rozenblat on 3/18/18.
+//  Copyright © 2018 roman rozenblat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
